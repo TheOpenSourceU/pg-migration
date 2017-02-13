24f2641ff282674e1fdd509c1546c3cf8347d11b
Frank Villasenor <frank.villasenor@gmail.com>
2017-02-12 23:14:27 -0600

    dbInfo module with initial tests. More tests coming....

---
+++ test/dbInfo.js
@@ -0,0 +1,27 @@
+var assert = require('chai').assert;
+var conn = require('../lib/dbConnection')("postgresql://localhost:5432/pg_migrate");
+var dbInfo = require('../lib/dbInfo');
+
+describe('dbInfo', function () {
+  before(function() {
+
+  });
+
+  beforeEach(function() {
+
+  });
+
+  it('builds out table if missing', function () {
+    return conn
+      .none('drop table if exists pg_migration_dbinfo')
+      .then(function() {
+        console.log('dropped old table... running dbInfo');
+        return dbInfo(conn);
+      })
+      .then(function (resultObj) {
+        console.log('received results', resultObj);
+        assert.isDefined(resultObj);
+        assert.equal("0.0.0", resultObj.version);
+      });
+  });
+});
